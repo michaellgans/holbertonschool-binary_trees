8256919c3c3b3b629377f9fa4ea9ef119294b2a1
michaellgans <michaellgans@hotmail.com>
2023-08-24 14:33:51 -0700

    M - creates 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - finds the uncle of a node
+ * @node: pointer to node in question
+ * Return: pointer to uncle
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
+
+	if (node->parent == node->parent->parent->left)
+		return (node->parent->parent->right);
+
+	if (node->parent == node->parent->parent->right)
+		return (node->parent->parent->left);
+
+	return (NULL);
+}
