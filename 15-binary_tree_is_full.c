0e4223222d073ee79a711144082d61cb570ae769
michaellgans <michaellgans@hotmail.com>
2023-08-24 13:34:25 -0700

    M - creates task 15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - checks if a tree is full or not
+ * @tree: points to the root of a tree
+ * Return: 0 if tree doesn't exist
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (1); /* empty is considered full */
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
+
+	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+}
