7bd6397df839e66ab42d29374d50b9d4179d9ab2
michaellgans <michaellgans@hotmail.com>
2023-08-24 13:43:35 -0700

    M - creates task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,13 +9,13 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (1); /* empty is considered full */
+		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
-	if (tree->left == NULL || tree->right == NULL)
-		return (0);
+	if (tree->left != NULL && tree->right != NULL)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 
-	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (0);
 }
